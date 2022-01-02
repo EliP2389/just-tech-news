5c889648d4f694e0699ac536b38b962055addecd
Eli Pagan <mbk8689@gmail.com>
2022-01-01 20:15:21 -0500

    created login

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -50,6 +50,28 @@ router.post('/', (req, res) => {
     })
 });
 
+router.post('/login', (req, res) => {
+// expects {email: 'lernantion@gmail.com', password: 'password12334'}
+User.findOne({
+    where: {
+        email: req.body.email
+    }
+}).then(dbUserData => {
+    if(!dbUserData) {
+        res.status(400).json({ message: 'No user with that email address!' });
+        return;
+    }
+
+    // verify user
+    const vaildPassword = dbUserData.checkPassword(req.body.password);
+    if (!vaildPassword) {
+        res.status(400).json({ message: 'Incorrect password! '});
+        return;
+    }
+    res.json({ user: dbUserData, message: 'You are now logged in!' });
+});
+});
+
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
     // expects {username: 'Lernantion', email: 'lernantion@gmail.com', password: 'password1234'}
