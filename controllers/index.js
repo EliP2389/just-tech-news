69d6b393b3c52ea0903be40fd343b3046274450f
Eli Pagan <mbk8689@gmail.com>
2022-01-05 10:47:24 -0500

    fixed home-routes

--- controllers/index.js
+++ controllers/index.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 
-const homeRoutes = require('./home-routes.js.js');
-
-router.use('/api', apiRoutes);
+const homeRoutes = require('./home-routes.js');
 
 router.use('/', homeRoutes);
+
+router.use('/api', apiRoutes);
 
 router.use((req, res) => {
     res.status(404).end();
