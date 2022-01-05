b629817216856f900c08aafabdf8533d9a777abe
Eli Pagan <mbk8689@gmail.com>
2022-01-05 10:34:35 -0500

    fixed server.js

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -7,22 +7,22 @@ const sequelize = require('../../config/connection')
 router.get('/', (req, res) => {
     Post.findAll({
         attributes: [
-            'id',
-            'post_url',
-            'title',
-            'created_at',
-            [sequelize.literal('(SELECT COUNT (*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-        ],
+        'id', 
+        'post_url', 
+        'title', 
+        'created_at',
+        [sequelize.literal('(SELECT COUNT (*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
         order: [['created_at', 'DESC']],
         include: [
             {
-                model: Comment,
-                attributes: [
-                    'id',
-                    'comment_text',
-                    'post_id',
-                    'user_id',
-                    'created_at'
+               model: Comment,
+               attributes: [
+                   'id',
+                   'comment_text',
+                   'post_id',
+                   'user_id',
+                   'created_at'
                 ],
                 include: {
                     model: User,
@@ -35,17 +35,12 @@ router.get('/', (req, res) => {
             }
         ]
     })
-     .then(dbPostData => {
-         
-        //  pass a single post object into the homepage template
-        const posts = dbPostData.map(post => post.get({ plain: true }));
-        res.render('homepage', { posts });
-     })
-     .catch(err => {
-         console.log(err);
-         res.status(500).json(err);
-     });
-    });
+        .then(dbPostData => res.json(dbPostData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
 router.get('/:id', (req, res) => {
     Post.findOne({
@@ -53,13 +48,13 @@ router.get('/:id', (req, res) => {
             id: req.params.id
         },
         attributes: [
-            'id',
-            'post_url',
-            'title',
-            'created_at',
-            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-        ],
-
+        'id', 
+        'post_url', 
+        'title', 
+        'created_at',
+        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
+        
         include: [
             {
                 model: Comment,
@@ -69,12 +64,12 @@ router.get('/:id', (req, res) => {
                     'post_id',
                     'user_id',
                     'created_at'
-                ],
-                include: {
-                    model: User,
-                    attributes: ['username']
-                }
-            },
+                 ],
+                 include: {
+                     model: User,
+                     attributes: ['username']
+                 }
+             },
             {
                 model: User,
                 attributes: ['username']
@@ -112,12 +107,12 @@ router.post('/', (req, res) => {
 router.put('/upvote', (req, res) => {
     // custom static method created in models/Post.js
     Post.upvote(req.body, { Vote })
-        .then(updatedPostData => res.json(updatedPostData))
-        .catch(err => {
-            console.log(err);
-            res.status(400).json(err);
-        });
-});
+      .then(updatedPostData => res.json(updatedPostData))
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  });
 
 router.put('/:id', (req, res) => {
     Post.update(
